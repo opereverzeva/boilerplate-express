32c78c93883d59fa6c246e46ef89928f7cb4222c
Olga Pereverzeva <44942134+opereverzeva@users.noreply.github.com>
2019-07-29 12:04:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var myApp = require('./myApp');
 var express = require('express');
 var app = express();
 
+console.log('Hello World');
+
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function(req, res, next) {
     var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
